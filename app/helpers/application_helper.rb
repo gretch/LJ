ba5112090a9c96e3d79f58260151de5b03fe35b6
Hendy Tanata <htanata@gmail.com>
2009-01-11 21:45:30 +0800

    Use blueprint forms.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,8 @@ module ApplicationHelper
   def flash_messages
     messages = []
     %w(notice warning error).each do |msg|
-      messages << content_tag(:div, html_escape(flash[msg.to_sym]), :id => "flash-#{msg}") unless flash[msg.to_sym].blank?
+      messages << content_tag(:div, html_escape(flash[msg.to_sym]),
+                              :id => "flash-#{msg}", :class => msg) unless flash[msg.to_sym].blank?
     end
     messages
   end
