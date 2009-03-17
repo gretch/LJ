59b2e7e75b803d80a916b1ad5b144d87fabd4de9
Hendy Tanata <htanata@gmail.com>
2009-03-17 16:33:42 +0800

    Use formtastic.

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
   config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com'
+  config.gem "justinfrench-formtastic", :lib => 'formtastic', :source  => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
   
   # These cause problems with irb. Left in for reference
