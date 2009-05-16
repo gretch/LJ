c73c505387a28bd5bad4df126fcac8bd5123fdc6
Hendy Tanata <htanata@gmail.com>
2009-05-16 15:57:29 +0800

    bortissimo_scaffold inherits from rspec_scaffold.

--- vendor/plugins/bortissimo_generators/generators/bortissimo_scaffold/bortissimo_scaffold_generator.rb
+++ vendor/plugins/bortissimo_generators/generators/bortissimo_scaffold/bortissimo_scaffold_generator.rb
@@ -1,38 +1,4 @@
-require File.dirname(__FILE__) + '/../bortissimo_default_values'
-
-class BortissimoScaffoldGenerator < Rails::Generator::NamedBase
-  default_options :skip_migration => false
-
-  attr_reader   :controller_name,
-                :controller_class_path,
-                :controller_file_path,
-                :controller_class_nesting,
-                :controller_class_nesting_depth,
-                :controller_class_name,
-                :controller_singular_name,
-                :controller_plural_name,
-                :resource_edit_path,
-                :default_file_extension
-  alias_method  :controller_file_name,  :controller_singular_name
-  alias_method  :controller_table_name, :controller_plural_name
-
-  def initialize(runtime_args, runtime_options = {})
-    super
-
-    @controller_name = @name.pluralize
-
-    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
-    @controller_class_name_without_nesting, @controller_singular_name, @controller_plural_name = inflect_names(base_name)
-
-    if @controller_class_nesting.empty?
-      @controller_class_name = @controller_class_name_without_nesting
-    else
-      @controller_class_name = "#{@controller_class_nesting}::#{@controller_class_name_without_nesting}"
-    end
-
-    @default_file_extension = "html.erb"
-  end
-
+class BortissimoScaffoldGenerator < RspecScaffoldGenerator
   def manifest
     record do |m|
 
@@ -121,32 +87,7 @@ class BortissimoScaffoldGenerator < Rails::Generator::NamedBase
       "Usage: #{$0} rspec_scaffold ModelName [field:type field:type]"
     end
 
-    def add_options!(opt)
-      opt.separator ''
-      opt.separator 'Options:'
-      opt.on("--skip-migration",
-             "Don't generate a migration file for this model") { |v| options[:skip_migration] = v }
-    end
-
     def scaffold_views
       %w[ index show new edit _form ]
     end
-
-    def model_name
-      class_name.demodulize
-    end
-end
-
-module Rails
-  module Generator
-    class GeneratedAttribute
-      def input_type
-        @input_type ||= case type
-          when :text                        then "textarea"
-          else
-            "input"
-        end
-      end
-    end
-  end
 end
